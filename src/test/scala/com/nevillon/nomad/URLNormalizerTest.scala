3f7c07f7332b50ac428d7dce2a148f4bdecf6f54
Vadym Bartko <hudvin@gmail.com>
2013-02-25 07:31:05 -0500

    headers for test/
    --HG--
    branch : v0.3

--- src/test/scala/com/nevillon/nomad/URLNormalizerTest.scala
+++ src/test/scala/com/nevillon/nomad/URLNormalizerTest.scala
@@ -1,3 +1,13 @@
+/**
+ * Copyright (C) 2012-2013 Vadim Bartko (vadim.bartko@nevilon.com).
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * See file LICENSE.txt for License information.
+ */
 package com.nevillon.nomad
 
 import org.junit.{Assert, Test}
