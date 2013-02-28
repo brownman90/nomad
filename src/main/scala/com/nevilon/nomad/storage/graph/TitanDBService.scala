eb81bb4aa4fed806e77818a6df1a626faea06b70
hudvin <hudvin@gmail.com>
2013-02-28 11:25:01 -0500

    sync block in dbservice

--- src/main/scala/com/nevilon/nomad/storage/graph/TitanDBService.scala
+++ src/main/scala/com/nevilon/nomad/storage/graph/TitanDBService.scala
@@ -49,79 +49,90 @@ trait TitanDBService extends Logs {
 
 
   def linkUrls(relations: List[Relation]) {
-    withTransaction {
-      implicit tx => {
+    synchronized {
+      withTransaction {
+        implicit tx => {
 
-        def getOrCreate(url: Url): Vertex = {
-          getUrlInTx(url.location) match {
-            case Some(v) => v
-            case None => saveOrUpdateUrlInTx(url)
+          def getOrCreate(url: Url): Vertex = {
+            getUrlInTx(url.location) match {
+              case Some(v) => v
+              case None => saveOrUpdateUrlInTx(url)
+            }
           }
+
+          relations.foreach(relation => {
+            val parentPage = getOrCreate(relation.from)
+            val childPage = getOrCreate(relation.to)
+            tx.addEdge(UUID.randomUUID().toString, parentPage, childPage, "relation")
+          })
+
         }
-
-        relations.foreach(relation => {
-          val parentPage = getOrCreate(relation.from)
-          val childPage = getOrCreate(relation.to)
-          tx.addEdge(UUID.randomUUID().toString, parentPage, childPage, "relation")
-        })
-
       }
     }
   }
 
 
   def saveOrUpdateUrl(url: Url) = {
-    withTransaction {
-      implicit tx => {
-        saveOrUpdateUrlInTx(url)
+    synchronized {
+      withTransaction {
+        implicit tx => {
+          saveOrUpdateUrlInTx(url)
+        }
       }
     }
   }
 
 
   def getBFSLinks(url: String, limit: Int): List[Url] = {
-    withTransaction[List[Url]] {
-      implicit tx => {
-        val rootVertex = getUrlInTx(url).get
-        new BFSTraverser(rootVertex, limit).traverse()
+    synchronized {
+      withTransaction[List[Url]] {
+        implicit tx => {
+          val rootVertex = getUrlInTx(url).get
+          new BFSTraverser(rootVertex, limit).traverse()
+        }
       }
     }
   }
 
   def getUrl(url: String): Option[Vertex] = {
-    withTransaction[Option[Vertex]] {
-      implicit tx =>
-        getUrlInTx(url)
+    synchronized {
+      withTransaction[Option[Vertex]] {
+        implicit tx =>
+          getUrlInTx(url)
+      }
     }
   }
 
   private def saveOrUpdateUrlInTx(url: Url)(implicit tx: TitanTransaction): Vertex = {
-    val vertex = {
-      getUrlInTx(url.location) match {
-        case None => tx.addVertex()
-        case Some(v) => v
+    synchronized {
+      val vertex = {
+        getUrlInTx(url.location) match {
+          case None => tx.addVertex()
+          case Some(v) => v
+        }
       }
+
+      vertex.setProperty("status", url.status.toString)
+      vertex.setProperty("location", url.location)
+      vertex.setProperty("fileId", url.fileId)
+
+      vertex
     }
-
-    vertex.setProperty("status", url.status.toString)
-    vertex.setProperty("location", url.location)
-    vertex.setProperty("fileId", url.fileId)
-
-    vertex
   }
 
   private def getUrlInTx(url: String)(implicit tx: TitanTransaction): Option[Vertex] = {
-    val vertices = tx.getVertices("location", url)
-    if (vertices.isEmpty) None
-    else if (vertices.size > 1)
-      throw new RuntimeException("There are more than one page with this url!")
-    else Some(vertices.iterator.next())
-
+    synchronized {
+      val vertices = tx.getVertices("location", url)
+      if (vertices.isEmpty) None
+      else if (vertices.size > 1)
+        throw new RuntimeException("There are more than one page with this url!")
+      else Some(vertices.iterator.next())
+    }
   }
 
 
   private def withTransaction[T](f: TitanTransaction => T): T = {
-    val tx = graph.startTransaction()
+      val tx = graph.startTransaction()
     try {
       val result = f(tx)
       tx.stopTransaction(Conclusion.SUCCESS)
